1fe32f7f7bc56f4177ba15297195eec297153469
SasikalaThennarasu5 <sasikalathennarasu5@gmail.com>
2026-02-05 22:36:34 +0530

    Update settings.py

--- vcs_project/settings.py
+++ vcs_project/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -166,6 +167,7 @@ SOCIALACCOUNT_QUERY_EMAIL = True
 SOCIALACCOUNT_LOGIN_ON_GET = True
 
 SITE_ID = 1
+
 
 
 
