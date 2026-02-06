dc8ffc64078002b32eb35979a8f7472043a6d0c4
SasikalaThennarasu5 <sasikalathennarasu5@gmail.com>
2026-02-06 13:38:14 +0530

    Update settings.py

--- vcs_project/settings.py
+++ vcs_project/settings.py
@@ -171,6 +171,17 @@ SOCIALACCOUNT_LOGIN_ON_GET = True
 
 SITE_ID = 1
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = "sasikalathennarasu5@gmail.com"
+EMAIL_HOST_PASSWORD = "stzygkmkksfqkgjb"
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
 
 
